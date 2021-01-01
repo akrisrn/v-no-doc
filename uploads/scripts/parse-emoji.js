1a1b89f50c9df758c2b0fd6cbcbacabb9bd10296
AkrISrn <shairkarine@gmail.com>
2021-01-01 13:43:32 +0800

    update parse-emoji.js

--- uploads/scripts/parse-emoji.js
+++ uploads/scripts/parse-emoji.js
@@ -1,5 +1,5 @@
 (() => {
-  const parseEmoji = () => vno.waitFor(() => twemoji.parse(document.body));
-  parseEmoji();
-  document.addEventListener(vno.enums.EEvent.htmlChanged, parseEmoji);
+  vno.callAndListen(() => vno.waitFor(() => {
+    twemoji.parse(document.body);
+  }), vno.enums.EEvent.htmlChanged);
 })();
