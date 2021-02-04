219bab22e4262d19e0500ce964d93a6d4d3d6009
AkrISrn <shairkarine@gmail.com>
2021-02-04 21:50:01 +0800

    fix common.js

--- uploads/scripts/common.js
+++ uploads/scripts/common.js
@@ -12,24 +12,28 @@
   consoleLink.href = `#/${vno.appSelf.selectConf}/console.md`;
   top.insertBefore(consoleLink, apiLink);
   vno.updateDom().then();
-  vno.callAndListen(() => {
-    if (vno.filePath.split('/')[2] === 'api' && !vno.mainSelf.isError) {
-      document.querySelectorAll('header,h2,h3,h4,h5,h6,#toc').forEach(element => {
-        element.classList.add('api');
-      });
-    }
-  }, vno.enums.EEvent.htmlChanged);
+
   vno.callAndListen(() => {
     const itemCommit = document.querySelector('.item-commit');
-    if (itemCommit) {
-      const hash = itemCommit.innerText;
-      const a = document.createElement('a');
-      a.href = `https://raw.githubusercontent.com/akrisrn/v-no-doc/${hash}${vno.filePath}`;
-      a.target = '_blank';
-      a.rel = 'noopener noreferrer';
-      a.text = hash;
-      itemCommit.innerHTML = '';
-      itemCommit.append(a);
+    if (!itemCommit) {
+      return;
     }
+    const hash = itemCommit.innerText;
+    const a = document.createElement('a');
+    a.href = `https://raw.githubusercontent.com/akrisrn/v-no-doc/${hash}${vno.filePath}`;
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+    a.text = hash;
+    itemCommit.innerHTML = '';
+    itemCommit.append(a);
   }, vno.enums.EEvent.mainShown);
+
+  vno.callAndListen(() => {
+    if (vno.filePath.split('/')[2] !== 'api' || vno.mainSelf.isError) {
+      return;
+    }
+    document.querySelectorAll('header,h2,h3,h4,h5,h6,#toc').forEach(element => {
+      element.classList.add('api');
+    });
+  }, vno.enums.EEvent.htmlChanged);
 })();
